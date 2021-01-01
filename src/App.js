f6fa264c03a4d770fe2dc881cf51fa60a296beb1
Sandeep Kumar <sandeepypb@gmail.com>
2021-01-01 21:21:14 +0530

    create project and add auth with firebase

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Signup from "./auth/Signup";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Bookmark Application
+        <Signup />
+      </header>
+    </div>
+  );
+}
+
+export default App;
